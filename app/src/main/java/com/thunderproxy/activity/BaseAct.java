58c3e372d4be38856e11234d26bb13b4d9b378a9
6a209 <6a209@6a209s-Mac-Pro.lan>
2016-01-15 00:52:47 +0800

    init

---
+++ app/src/main/java/com/thunderproxy/activity/BaseAct.java
@@ -0,0 +1,27 @@
+package com.thunderproxy.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 6a209 on 16/1/14.
+ */
+public class BaseAct extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+    }
+
+}
+
---
+++ app/src/main/java/com/thunderproxy/activity/IndexAct.java
@@ -0,0 +1,17 @@
+package com.thunderproxy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.thunderproxy.R;
+
+public class IndexAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+
+
+}
